8ae142a244490c8b5a2415395a061ca08b879d69
prenit-wankhede <prenit.wankhede.me@gmail.com>
2020-04-04 13:41:25 +0530

    use import python_2_unicode_compatible from six

--- cities_light/abstract_models.py
+++ cities_light/abstract_models.py
@@ -5,7 +5,8 @@ import re
 import autoslug
 import pytz
 
-from django.utils.encoding import python_2_unicode_compatible
+# from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 from django.db import models
 from django.db.models import lookups
